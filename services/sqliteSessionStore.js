4e40229e5072b832f50fe54505774a99f1177429
Yogiraj139 <kn727722@gmail.com>
2026-04-23 14:01:58 +0530

    Delete services directory

--- services/sqliteSessionStore.js
+++
@@ -1,4 +0,0 @@
-﻿// Kept for project compatibility. This package uses express-session's built-in store for single-instance Railway deployments.
-export function createSessionStore() {
-  return undefined;
-}
